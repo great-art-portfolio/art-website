694c914be3f07fe4883c440fa92214cfab6dac5c
Chris Straka <45607257+chris-straka@users.noreply.github.com>
2025-03-23 18:53:05 -0600

    save again

--- functions/admin-oauth.ts
+++ functions/admin-oauth.ts
@@ -1,9 +1,12 @@
-/**
- * Visits /admin -> github login -> this function -> give client token (to manipulate github repo)
- * @param {*} context 
- * @returns 
- */
+// Visits /admin -> github login -> this function -> give client token (to manipulate github repo)
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   const { state, code } = context.params
   return Response.redirect(`/admin?code=${code}&state=${state}`, 302);
 }
+
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+  // const { code } = await context.request.json();
+  // Exchange code for token with GitHub, return token
+
+  return new Response();
+};
